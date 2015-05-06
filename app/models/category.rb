99fe756b51915c2e2c140248f2a65c341799f142
Ron Schnellbacher <rjschnell.tech@sbcglobal.net>
2015-05-06 19:14:26 -0400

    WIP - More stuff after pull from dave.

--- app/models/category.rb
+++
@@ -1,8 +0,0 @@
-class Category < ActiveRecord::Base
-  has_many :posts
-  validates :name, presence: true, length: { maximum: 20 }
-  validates :title, length: { maximum: 100 }
-  validates :description, length: { maximum: 500 }
-  validates :sidebar, length: { maximum: 5120 }
-  validates :submission_text, length: { maximum: 1024 }
-end
